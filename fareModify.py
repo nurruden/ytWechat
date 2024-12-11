8fd12ee53706bdf40ee3bef2e3aba5e220d99b4a
告别可乐 <oncwnuPm775OkvZ32HQDBrKHUq1c@git.weixin.qq.com>
2024-12-11 09:42:20 +0800

    optimizw fare modify

--- fareModify.py
+++ fareModify.py
@@ -14,15 +14,19 @@ from copy import deepcopy
 from datetime import datetime
 
 
+# 初始化日志
 log = logger(functionName=__name__)
 
-wechat = configparser.ConfigParser()
-wechat.read('./conf/wechat.ini')
+# 加载配置文件
+def load_config(file_path):
+    config = configparser.ConfigParser()
+    config.read(file_path)
+    return config
 
+wechat = load_config('./conf/wechat.ini')
+easConf = load_config('./conf/eas.ini')
 
-easConf = configparser.ConfigParser()
-easConf.read('./conf/eas.ini')
-#
+# 读取API配置
 appKey = easConf['basic']['appKey']
 appSecret = easConf['basic']['appSecret']
 url = easConf['basic']['url']
@@ -31,83 +35,110 @@ fareAdd = easConf['url']['fareAdd']
 fareModify = easConf['url']['fareModify']
 fareDel = easConf['url']['fareDel']
 
-
+# Excel数据处理配置
 file_path = '汇总表.xlsx'
 sheet_name = '汇总表'
+
+# 定义字段映射表
+dicValue = {
+    '纸袋': 'zhiDai',
+    '吨袋': 'dunDai',
+    '海运费(CNY)': 'ckhyf',
+    'Pallet&Wrap/MT打托缠膜（人民币/吨）': 'bjdaTuoChanMo',
+    '海运费': 'ckhyfmy',
+    '出口港杂费': 'ckgzf',
+    '内陆运费总额（人民币）': 'nlyfck',
+    '摘要': 'description'
+}
+valueDict = {v: k for k, v in dicValue.items()}  # 反向映射
+
+columns = [
+    '销售员', '产品包装', 'PackagingCOST/MT包装费（人民币/吨）',
+    'Pallet&Wrap/MT打托缠膜（人民币/吨）', '海运费(CNY)', '海运费',
+    '港杂费', '额外费用', '内陆运费总额（人民币）', '状态', '数量（吨）', '摘要'
+]
+
+# 初始化数据处理器
 processor = ExcelDataProcessor(file_path, sheet_name)
-dicValue={'纸袋':'zhiDai','吨袋':'dunDai','海运费(CNY)':'ckhyf','Pallet&Wrap/MT打托缠膜（人民币/吨）':'bjdaTuoChanMo','海运费':'ckhyfmy','出口港杂费':'ckgzf','内陆运费总额（人民币）':'nlyfck','摘要':'description'}
-# 出口港杂费=港杂费+额外费用
-valueDict={'zhiDai':'纸袋','dunDai':'吨袋','ckhyf':'海运费(CNY)','bjdaTuoChanMo':'Pallet&Wrap/MT打托缠膜（人民币/吨）','ckhyfmy':'海运费','ckgzf':'出口港杂费','nlyfck':'内陆运费总额（人民币）','description':'摘要'}
-columns = ['销售员', '产品包装', 'PackagingCOST/MT包装费（人民币/吨）', 'Pallet&Wrap/MT打托缠膜（人民币/吨）', '海运费(CNY)', '海运费', '港杂费', '额外费用',
-           '内陆运费总额（人民币）','状态','数量（吨）','摘要']
 result = processor.process_data(columns)
+print(result)
 order_manager = OrderManager(url)
-for i in result:
-    if '系统号' not in i.keys():
-        pass
-    copyDic=deepcopy(i)
-    number=i['系统号']
-    print(number)
-    i.pop('系统号')
-    status=i['状态']
-    i.pop('状态')
-    saler=i['销售员']
-    i.pop('销售员')
-    if '纸袋' in i['产品包装']:
-        value=i['PackagingCOST/MT包装费（人民币/吨）']*i['数量（吨）']
-        i.pop('PackagingCOST/MT包装费（人民币/吨）')
-        i.pop('产品包装')
-        i['纸袋']=value
-    elif '吨袋' in i['产品包装']:
-        value=i['PackagingCOST/MT包装费（人民币/吨）']*i['数量（吨）']
-        i.pop('PackagingCOST/MT包装费（人民币/吨）')
-        i.pop('产品包装')
-        i['吨袋']=value
-    i['出口港杂费']=i['港杂费']+i['额外费用']
-    i.pop('额外费用')
-    i.pop('港杂费')
-    print(copyDic)
-    print(i)
-    des={}
-    for t in i.keys():
-        if i[t]!= 0:
-            des[t]=i[t]
-    des.pop('数量（吨）')
-    print(des)
-    currentDateAndTime = datetime.now()
-    currentTime = currentDateAndTime.strftime("%Y/%m/%d %H:%M")
-    if status=='新增':
-        tmp={}
-        for j in dicValue.keys():
-            if j in des.keys():
-                tmp[dicValue[j]]=des[j]
-        tmp['description']=str(currentTime)+str(des)
-        print(tmp)
-        # res=order_manager.postOrder(fareAdd,number,**tmp)
-        # print(res)
+
+# 处理包装费用
+def process_packaging(data):
+    packaging_type = data.pop('产品包装', None)
+    quantity = data.pop('数量（吨）', 0)
+    cost_per_unit = data.pop('PackagingCOST/MT包装费（人民币/吨）', 0)
+
+    if '纸袋' in packaging_type:
+        data['纸袋'] = cost_per_unit * quantity
+    elif '吨袋' in packaging_type:
+        data['吨袋'] = cost_per_unit * quantity
+    return data
+
+# 处理费用字段
+def process_fees(data):
+    data['出口港杂费'] = data.pop('港杂费', 0) + data.pop('额外费用', 0)
+    return data
+
+# 根据状态执行对应的操作
+def handle_status(record, status, number):
+    current_time = datetime.now().strftime("%Y/%m/%d %H:%M")
+
+    if status == '新增':
+        data = {dicValue[k]: v for k, v in record.items() if v != 0}
+        des={k:v for k,v in record.items() if v !=0 }
+        print(des)
+        data['description'] = f"{current_time} || {des}"
+        print(data)
+        # log.info(f"新增记录: {data}")
+        # Uncomment to make API call
+        # response = order_manager.postOrder(fareAdd, number, **data)
+        # log.info(response)
+
     elif status == '修改':
-        m = order_manager.getOrder(pageNum=2, page=1, number=number)
-        print(m)
-        tmp = {}
-        for j in dicValue.keys():
-            if j in i.keys():
-                tmp[dicValue[j]] = i[j]
-        print(tmp)
-        finaldic={}
-        desc={}
-        for x in tmp.keys():
-            if m['sysnList'][0][x]!=str(tmp[x]):
-                finaldic[x]=tmp[x]
-                desc[valueDict[x]]=tmp[x]
-        print(finaldic)
-        if '摘要' in desc.keys():
-            oldDesc=desc['摘要']
-            desc.pop('摘要')
-            newDes=str(currentTime)+str(desc)+str('|')+str(oldDesc)
-        finaldic['description']=newDes
-        print(finaldic)
-    #     modify=order_manager.postOrder(fareModify,number=number,**finaldic)
-    #     print(modify)
-    #
+        existing_data = order_manager.getOrder(pageNum=2, page=1, number=number)
+        log.info(f"现有记录: {existing_data}")
+
+        modified_data = {}
+        description = {}
+
+        for key, value in record.items():
+            if existing_data['sysnList'][0][dicValue[key]] != str(value):
+                modified_data[dicValue[key]] = value
+                description[valueDict[dicValue[key]]] = value
+
+        if '摘要' in description:
+            old_desc = description.pop('摘要')
+            description = f"{current_time} {description} || {old_desc}"
+        modified_data['description'] = description
+        log.info(f"修改记录: {modified_data}")
+        # Uncomment to make API call
+        response = order_manager.postOrder(fareModify, number, **modified_data)
+        print(response)
+
+# 主处理逻辑
+def main():
+    for record in result:
+        if '系统号' not in record:
+            continue
+
+        number = record.pop('系统号')
+        status = record.pop('状态', None)
+        record.pop('销售员', None)
+
+        record = process_packaging(record)
+        record = process_fees(record)
+        log.info(f"处理后的记录: {record}")
+
+        handle_status(record, status, number)
+
+# 执行主程序
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        log.error(f"程序执行时发生错误: {e}")
+
 
 
